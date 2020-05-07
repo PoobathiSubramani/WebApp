063a3cb1d35d13cb7c35ec870f5be5f91f4b1678
Poobathi <boopathy.s@gmail.com>
2020-05-07 20:35:43 +0530

    v1.01
    moved server.js to backend folder

--- myapp/backend/server.js
+++ myapp/backend/server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const app = require('./backend/app'); //importing express app
+const app = require('./app'); //importing express app
 const port = process.env.PORT || 3000;
 app.set('port', port); //set the port for express app
 const server = http.createServer(app); //invoke express in the server
